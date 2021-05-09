d1a0cc15267edad7712d7dc5bc18024bcfb0b354
Yashira Baggett <Yashira@Yashiras-Air.attlocal.net>
2021-05-09 07:01:06 -0700

    adding html and js, added psuedocode file

---
+++ hangman.js
@@ -0,0 +1,16 @@
+//create a way for the guessing player (human) to enter their choices
+//I'll use a prompt for this 
+
+const name = prompt("What's your name?");
+console.log(`Hello ${name}`)
+
+//Game structure
+//Choose a random word
+//Take the player's guess
+//Quit the game if the player wants to 
+//Check that the player's guess is a valid letter
+//Keep track of letters the player has guessed
+//Show the player their progress
+//Finish when the player has guessed the word
+
+
